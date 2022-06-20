bacb67253fb6c9f985809e8efdcaa7dfe5fad339
Pierce <ppuckette@gmail.com>
2022-06-20 18:41:24 -0400

    Display items  - buttons - dynamic buttons

--- setup/src/Categories.js
+++ setup/src/Categories.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({ categories, filterItems }) => {
+	return (
+		<div className="btn-container">
+			{/* dynamically map categories to each item in the menu and assign it a buttom */}
+			{categories.map((category, index) => {
+				// create button for all the different categories
+				return (
+					<button
+						type="button"
+						className="filter-btn"
+						key={index}
+						onClick={() => filterItems(category)}>
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Categories;
